8b7cfcd38f8785283b0caf489bf98f7a8823c47d
hariprasad <hariprasad.alagesan@gmail.com>
2026-03-04 17:55:42 +0530

    added db url

--- Backend/app/app/db/session.py
+++ Backend/app/app/db/session.py
@@ -1,8 +1,15 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-dburl=""
-engine=create_engine(dburl)
+dburl="mysql://4WUWyWmxdkEPJB7.root:BSGkzQO4R1Vb1R5O@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/Lead_Management"
+engine = create_engine(dburl,
+    connect_args={
+        "ssl": {
+            "ca": "path/to/ca-cert.pem"
+        }
+    }
+)
+
 SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
