ce339991aa623d6066b9e1349739309775222229
hariprasad <hariprasad.alagesan@gmail.com>
2026-03-04 18:02:20 +0530

    added meta data

--- Backend/app/app/db/base_class.py
+++ Backend/app/app/db/base_class.py
@@ -1,2 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
+from session import engine
 Base= declarative_base()
+
+
+
+if __name__==__name__:
+    Base.metadata.create_all(bind=engine)
