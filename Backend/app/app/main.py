94ad274667def8b24c6d4d2b489a04f403960dea
hariprasad <hariprasad.alagesan@gmail.com>
2026-03-05 14:01:49 +0530

    added cors

--- Backend/app/app/main.py
+++ Backend/app/app/main.py
@@ -4,10 +4,16 @@ from app.db.base_class import Base
 from app.api.endpoints import users
 from app.api.endpoints import lead
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+
+app.add_middleware(CORSMiddleware
+                   ,allow_origins=["*"],allow_credentials=["*"],
+                   allow_method=["*"],allow_headers=["*"])
 app.include_router(users.router)
 app.include_router(lead.router)
+
 
 
 if __name__ == "__main__":
