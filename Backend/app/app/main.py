33a5e0ee2cc582afb4c4d3933a8c1c8ac17ac4c2
hariprasad <hariprasad.alagesan@gmail.com>
2026-03-06 13:09:00 +0530

    added cors

--- Backend/app/app/main.py
+++ Backend/app/app/main.py
@@ -7,10 +7,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-
+"""
 app.add_middleware(CORSMiddleware
                    ,allow_origins=["*"],allow_credentials=["*"],
-                   allow_methods=["*"],allow_headers=["*"])
+                   allow_methods=["*"],allow_headers=["*"])"""
+
+
+origins = ["http://localhost:5174"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,allow_credentials=True,
+    allow_methods=["*"],allow_headers=["*"])
 
 app.include_router(users.router)
 app.include_router(lead.router)
