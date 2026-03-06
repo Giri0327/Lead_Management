7c6b75541d64f6f8564d8c8ae85dd48d3faf9827
hariprasad <hariprasad.alagesan@gmail.com>
2026-03-06 11:31:08 +0530

    added in session

--- Backend/app/app/db/session.py
+++ Backend/app/app/db/session.py
@@ -4,8 +4,16 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
-url="mysql+pymysql://4WUWyWmxdkEPJB7.root:BSGkzQO4R1Vb1R5O@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/Lead_Management"
-#DATABASE_URL=url
+# url="mysql+pymysql://4WUWyWmxdkEPJB7.root:BSGkzQO4R1Vb1R5O@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/Lead_Management"
+# #DATABASE_URL=url
+
+# # get current file directory
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# # go one folder up to find .env
+# env_path = os.path.join(BASE_DIR, "..", ".env")
+
+# load_dotenv(env_path)
 cert_path = os.getenv("ca")
 
 engine = create_engine(DATABASE_URL,
