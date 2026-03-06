c9b5823db82d2897f2ad03241d86b72f2333c9cc
Keerthi <keerthikk0302@gmail.com>
2026-03-06 11:40:32 +0530

    cmd

--- Backend/app/app/db/session.py
+++ Backend/app/app/db/session.py
@@ -4,20 +4,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
-# url="mysql+pymysql://4WUWyWmxdkEPJB7.root:BSGkzQO4R1Vb1R5O@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/Lead_Management"
-# #DATABASE_URL=url
 
-# # get current file directory
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# # go one folder up to find .env
-# env_path = os.path.join(BASE_DIR, "..", ".env")
-
-# load_dotenv(env_path)
 cert_path = os.getenv("ca")
-
-#print("DATABASE_URL:", DATABASE_URL)
-#print("CA:", cert_path)
 
 engine = create_engine(DATABASE_URL,
     connect_args={
