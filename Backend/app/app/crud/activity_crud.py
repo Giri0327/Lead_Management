3da951cacc01bc74f06a599285c3d93fdab9e832
Keerthi <keerthikk0302@gmail.com>
2026-03-11 09:50:46 +0530

    K

---
+++ Backend/app/app/crud/activity_crud.py
@@ -0,0 +1,25 @@
+# from fastapi import HTTPException
+# from app.schema import Lead_Activites_Schema
+# from app.models import Lead_Activity
+# from app.models.Lead_Table import Lead
+# from app.models.User_Table import User
+# from sqlalchemy import func
+
+# class Activity:
+#     def __init__(self,activity,db):
+#         self.activity=activity
+#         self.db=db
+        
+
+#     def add_activity(self):
+#         activity = Activity(
+
+#             Lead_ID = self.activity.lead_id,
+#             User_ID = self.activity.user_id,
+#             Notes = self.activity.notes,
+#             Scheduled_On = self.activity.scheduled_on,
+            
+
+
+
+#         )
